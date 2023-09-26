72bfa4cc9e8a6f0c7dea3fae481832f31744e4f1
pmr28 <91907307+pmr28@users.noreply.github.com>
2023-09-26 18:09:47 -0400

    Added PaulR credit

--- slugout/src/credits/credits.rs
+++ slugout/src/credits/credits.rs
@@ -16,12 +16,16 @@ pub fn load_credit_texture(mut commands: Commands){
 
 pub fn cycle_credits(mut update_screen: Query<&mut Handle<Image>, With<Credits>>, keyboard: Res<Input<KeyCode>>, asset_server: Res<AssetServer>){
     // Add your images here
-    let credits_lib = vec!["Crystal.png"];
+    let credits_lib = vec!["Crystal.png", "PaulR.png"];
     for(mut handle) in update_screen.iter_mut(){ 
         // Credits for everyone from number 1 - 9
         if keyboard.just_pressed(KeyCode::Key1){
             *handle = asset_server.load(credits_lib[0]);
         }
+        if keyboard.just_pressed(KeyCode::Key2){
+            *handle = asset_server.load(credits_lib[1]);
+        }
+        
         // if keyboard.just_pressed(KeyCode::Key2){
         //     *handle = asset_server.load(credits_lib[1]);
         // }
