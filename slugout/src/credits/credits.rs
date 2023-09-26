925f95cbecb921ba079d70629d4521da8c9f6ad5
Alex Zhou <alexzhoulol@gmail.com>
2023-09-26 15:48:25 -0400

    png shuffle basic done

---
+++ slugout/src/credits/credits.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+
+#[derive(Component)]
+pub struct Credits;
+
+// Load credit screen assets
+pub fn load_credit_texture(mut commands: Commands){
+    // Spawn credits sprite
+    commands.spawn(SpriteBundle {
+        transform: Transform::from_xyz(0., 0., 0.),
+        ..default()
+    }).insert(Credits);
+  
+}
+
+pub fn cycle_credits(mut update_screen: Query<&mut Handle<Image>, With<Credits>>, keyboard: Res<Input<KeyCode>>, asset_server: Res<AssetServer>){
+    // Add your images here
+    let credits_lib = vec!["Crystal.png"];
+    for(mut handle) in update_screen.iter_mut(){ 
+        // Credits for everyone from number 1 - 9
+        if keyboard.just_pressed(KeyCode::Key1){
+            *handle = asset_server.load(credits_lib[0]);
+        }
+        // if keyboard.just_pressed(KeyCode::Key2){
+        //     *handle = asset_server.load(credits_lib[1]);
+        // }
+    }
+}
--- slugout/src/main.rs
+++ slugout/src/main.rs
@@ -1,14 +1,11 @@
-use std::process::id;
-
-use bevy::{prelude::*, window::PresentMode, utils::petgraph::visit::EdgeRef};
+use bevy::{prelude::*, window::PresentMode, render::texture};
 
 mod credits;
-#[derive(Component, Deref, DerefMut)]
-struct PopupTimer(Timer);
 
 const WIN_W: f32 = 1280.0;
 const WIN_H: f32 = 720.0;
 const TITLE: &str = "Slugout";
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0., 1., 1.0)))
@@ -22,10 +19,12 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, setup)
-
+        .add_systems(Startup, credits::credits::load_credit_texture)
+        .add_systems(Update, credits::credits::cycle_credits)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 }
+
