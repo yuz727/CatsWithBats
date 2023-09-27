116ba96b3f08620f311e55060963955682ef63f3
jimmyll2 <99372016+jimmyll2@users.noreply.github.com>
2023-09-26 21:25:13 -0400

    Update credits.rs Jimmy

--- slugout/src/credits/credits.rs
+++ slugout/src/credits/credits.rs
@@ -16,7 +16,7 @@ pub fn load_credit_texture(mut commands: Commands){
 
 pub fn cycle_credits(mut update_screen: Query<&mut Handle<Image>, With<Credits>>, keyboard: Res<Input<KeyCode>>, asset_server: Res<AssetServer>){
     // Add your images here
-    let credits_lib = vec!["Alex_Credits.png", "PaulR.png", "JakobR.png", "brayden.png", "nicolecredit.png", "lgy2credits.png", "RafaelCredits.png"];
+    let credits_lib = vec!["Alex_Credits.png", "PaulR.png", "JakobR.png", "brayden.png", "nicolecredit.png", "lgy2credits.png", "RafaelCredits.png","Jimmy.png"];
     for(mut handle) in update_screen.iter_mut(){ 
         // Credits for everyone from number 1 - 9
         if keyboard.just_pressed(KeyCode::Key1){
@@ -39,6 +39,9 @@ pub fn cycle_credits(mut update_screen: Query<&mut Handle<Image>, With<Credits>>
         }
         if keyboard.just_pressed(KeyCode::Key7){
             *handle = asset_server.load(credits_lib[6]);
+        }
+        if keyboard.just_pressed(KeyCode::Key8){
+            *handle = asset_server.load(credits_lib[7]);
         }
     }
 }
