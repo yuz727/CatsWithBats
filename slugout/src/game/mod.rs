bd75f2a6a6a4ae072bc4f879a7987cee201bf009
Luke Charlesworth <lukecharlesworth@Lukes-MacBook-Pro.local>
2023-10-24 18:41:51 -0400

    trying to merge everything

---
+++ slugout/src/game/mod.rs
@@ -0,0 +1,113 @@
+use crate::components::*;
+use bevy::{prelude::*, window::PresentMode};
+mod ball;
+mod components;
+mod credits;
+mod npc;
+mod player;
+const WIN_W: f32 = 1280.0;
+const WIN_H: f32 = 720.0;
+const TITLE: &str = "Slugout";
+
+pub struct GamePlugin; 
+
+imp Plugin for GamePlugin { 
+    fn build(&self, app: &mut App) {
+        .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: TITLE.into(),
+                resolution: (WIN_W, WIN_H).into(),
+                present_mode: PresentMode::Fifo,
+                ..default()
+            }),
+            ..default()
+
+            .add_systems(Startup, setup)
+            .add_plugins(npc::npc::NPCPlugin)
+            .add_plugins(ball::ball::BallPlugin)
+            .add_systems(Update, player::player_movement::move_player)
+        }))
+   
+    }
+
+}
+
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("background1_small.png"),
+            transform: Transform::from_xyz(0., 0., 0.),
+            ..default()
+        })
+        .insert(Background);
+
+    // Load Player
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("Player.png"),
+            transform: Transform::with_scale(Transform::from_xyz(0., 0., 3.), Vec3::splat(0.13)),
+            ..default()
+        })
+        .insert(Player)
+        .insert(player::player_movement::PlayerVelocity::new())
+        .insert(Colliding::new());
+
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("Face.png"),
+            transform: Transform::with_scale(Transform::from_xyz(0., 0., 4.), Vec3::splat(0.13)),
+            ..default()
+        })
+        .insert(Face);
+
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("Bat.png"),
+            transform: Transform::with_scale(Transform::from_xyz(-5., 0., 4.), Vec3::splat(0.13)),
+            ..default()
+        })
+        .insert(Bat);
+
+    // Load Objects
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("SideTable.png"),
+            transform:
+                Transform::with_scale(Transform::from_xyz(120., 170., 2.), Vec3::splat(0.18)),
+            ..default()
+        })
+        .insert(Object);
+
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("TVStand.png"),
+            transform: Transform::with_rotation(
+                Transform::with_scale(Transform::from_xyz(0., -250., 2.), Vec3::splat(0.18)),
+                Quat::from_rotation_z(4.72),
+            ),
+            ..default()
+        })
+        .insert(Object);
+
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("Recliner.png"),
+            transform:
+                Transform::with_scale(Transform::from_xyz(-60., 210., 2.), Vec3::splat(0.18)),
+            ..default()
+        })
+        .insert(Object); 
+    commands 
+        .spawn(SpriteBundle {
+            texture: asset_server.load("Rug.png"),
+            transform: Transform::with_rotation(
+                Transform::with_scale(Transform::from_xyz(0., 0., 1.), Vec3::splat(0.6)),
+                Quat::from_rotation_z(1.56),
+            ),
+            ..default()
+        })
+        .insert(Rug{friction: 1.4,}); 
+}
