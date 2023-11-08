a97844219417a945ee627989e429f24aeef79b3e
lilyyoung <Lily.Young@kurrentlogic.com>
2023-11-07 22:54:07 -0500

    aim

--- slugout/src/game/ball.rs
+++ slugout/src/game/ball.rs
@@ -425,7 +425,7 @@ fn swing(
 
     if let Some(mouse_position) = window.single().physical_cursor_position(){
         //println!("Cursor is inside window {:?}", mouse_position);
-        if unsafe { MOUSE_BUTTON_JUST_RELEASED } {
+        //if unsafe { MOUSE_BUTTON_JUST_RELEASED } {
         //if unsafe { MOUSE_BUTTON_JUST_RELEASED } {
             for (mut ball, mut ball_velocity, mut ball_transform) in query.iter_mut() {
                 
@@ -484,7 +484,7 @@ fn swing(
                 BAT_TRANSFORMED = false;
             }
 
-        }
+        //}
 
 
     }
