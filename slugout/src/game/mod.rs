aa421fb5b94dc53f41b7db15586a89c59d835056
jjr113 <jjr113@pitt.edu>
2023-11-20 13:06:56 -0500

    Update mod.rs

--- slugout/src/game/mod.rs
+++ slugout/src/game/mod.rs
@@ -46,73 +46,79 @@ impl Plugin for GamePlugin {
 
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-
     // Load Player
     commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("Player.png"),
-            transform: Transform::with_scale(Transform::from_xyz(0., 0., 10.), Vec3::splat(0.13)),
-            ..default()
-        })
-        .insert(Player)
-        .insert(player_movement::PlayerVelocity::new())
-        .insert(Colliding::new());
+    .spawn(SpriteBundle {
+        texture: asset_server.load("Player.png"),
+        transform: Transform::with_scale(Transform::from_xyz(0., 0., 10.), Vec3::splat(0.13)),
+        ..default()
+    })
+    .insert(Player)
+    .insert(player_movement::PlayerVelocity::new())
+    .insert(Colliding::new());
 
     commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("Face.png"),
-            transform: Transform::with_scale(Transform::from_xyz(0., 0., 20.), Vec3::splat(0.13)),
-            ..default()
-        })
-        .insert(Face);
+    .spawn(SpriteBundle {
+        texture: asset_server.load("Face.png"),
+        transform: Transform::with_scale(Transform::from_xyz(0., 0., 20.), Vec3::splat(0.13)),
+        ..default()
+    })
+    .insert(Face);
 
     commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("Bat.png"),
-            transform: Transform::with_scale(Transform::from_xyz(-5., 0., 20.), Vec3::splat(0.13)),
-            ..default()
-        })
-        .insert(Bat);
+    .spawn(SpriteBundle {
+        texture: asset_server.load("Bat.png"),
+        transform: Transform::with_scale(
+            Transform::from_xyz(-5., 0., 20.),
+            Vec3::new(0.175, 0.175, 0.),
+        ),
+        ..default()
+    })
+    .insert(Bat);
 
     // Load Objects
     commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("SideTable.png"),
-            transform:
-                Transform::with_scale(Transform::from_xyz(120., 170., 2.), Vec3::splat(0.18)),
-            ..default()
-        })
-        .insert(Object);
+    .spawn(SpriteBundle {
+        texture: asset_server.load("SideTable.png"),
+        transform: Transform::with_scale(
+            Transform::from_xyz(120., 170., 2.),
+            Vec3::splat(0.18),
+        ),
+        ..default()
+    })
+    .insert(Object);
 
     commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("TVStand.png"),
-            transform: Transform::with_rotation(
-                Transform::with_scale(Transform::from_xyz(0., -250., 2.), Vec3::splat(0.18)),
-                Quat::from_rotation_z(4.72),
-            ),
-            ..default()
-        })
-        .insert(Object);
+    .spawn(SpriteBundle {
+        texture: asset_server.load("TVStand.png"),
+        transform: Transform::with_rotation(
+            Transform::with_scale(Transform::from_xyz(0., -250., 2.), Vec3::splat(0.18)),
+            Quat::from_rotation_z(4.72),
+        ),
+        ..default()
+    })
+    .insert(Object);
 
     commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("Recliner.png"),
-            transform:
-                Transform::with_scale(Transform::from_xyz(-60., 210., 2.), Vec3::splat(0.18)),
-            ..default()
-        })
-        .insert(Object); 
-    commands 
-        .spawn(SpriteBundle {
-            texture: asset_server.load("Rug.png"),
-            transform: Transform::with_rotation(
-                Transform::with_scale(Transform::from_xyz(0., 0., 1.), Vec3::splat(0.6)),
-                Quat::from_rotation_z(1.56),
-            ),
-            ..default()
-        })
-        .insert(Rug{friction: 1.4,}); 
+    .spawn(SpriteBundle {
+        texture: asset_server.load("Recliner.png"),
+        transform: Transform::with_scale(
+            Transform::from_xyz(-60., 210., 2.),
+            Vec3::splat(0.18),
+        ),
+        ..default()
+    })
+    .insert(Object);
+    commands
+    .spawn(SpriteBundle {
+        texture: asset_server.load("Rug.png"),
+        transform: Transform::with_rotation(
+            Transform::with_scale(Transform::from_xyz(0., 0., 1.), Vec3::splat(0.6)),
+            Quat::from_rotation_z(1.56),
+        ),
+        ..default()
+    })
+    .insert(Rug { friction: 1.4 });
     commands
     .spawn(SpriteBundle {
         texture: asset_server.load("newAim.png"),
@@ -121,4 +127,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     })
     .insert(Aim);
     commands.insert_resource(Events::<CursorMoved>::default());
+    commands
+    .spawn(SpriteBundle {
+        texture: asset_server.load("FullHealth.png"),
+        transform: Transform::with_scale(
+            Transform::from_xyz(525., 280., 2.),
+            Vec3::splat(0.11),
+        ),
+        ..default()
+    })
+    .insert(Health);
 }
