caa2c4321e15126173b0c7d45feba513ac131978
nvp14 <146030859+nvp14@users.noreply.github.com>
2023-12-05 19:18:05 -0500

    Added powerup flag

--- slugout/src/game/mod.rs
+++ slugout/src/game/mod.rs
@@ -173,6 +173,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             powerup: "none".to_string(),
             powerup_timer: 0.,
             health: 3,
+            powerup_applied: false,
         })
         .insert(player_movement::PlayerVelocity::new())
         .insert(Colliding::new());
